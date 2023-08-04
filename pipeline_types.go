ed90c418431d7cb2657f524c84663599bb4bf8aa
rcjames <reubenc.james@gmail.com>
2023-08-04 20:49:55 +0100

    chore: Create godocs for Pipeline types

--- pipeline_types.go
+++ pipeline_types.go
@@ -1,6 +1,11 @@
 package gocd
 
+// A Pipeline is a configuration for a [Pipeline].
+//
+// [Pipeline]: https://api.gocd.org/current/#the-pipeline-config-object
 type Pipeline struct {
+	// Links are metadata about requests returned by GoCD. This does not need
+	// to be provided.
 	Links                Links                         `json:"_links,omitempty"`
 	Group                string                        `json:"group,omitempty"`
 	LabelTemplate        string                        `json:"label_template,omitempty"`
@@ -16,18 +21,28 @@ type Pipeline struct {
 	Timer                *PipelineTimer                `json:"timer,omitempty"`
 }
 
+// A PipelineConfigRepo maps to a "[Config repo origin]" object.
+//
+// [Config repo origin]: https://api.gocd.org/current/#the-generic-tracking-tool-object
 type PipelineConfigRepo struct {
+	// Links are metadata about requests returned by GoCD. This does not need
+	// to be provided.
 	Links *Links `json:"_links,omitempty"`
 	Type  string `json:"type,omitempty"`
 	Id    string `json:"id,omitempty"`
 }
 
+// A PipelineParameter maps to a "[Pipeline parameter]" object.
+//
+// [Pipeline parameter]: https://api.gocd.org/current/#the-pipeline-parameter-object
 type PipelineParameter struct {
 	Name  string `json:"name,omitempty"`
 	Value string `json:"value,omitempty"`
 }
 
-// TODO - Validation on Value OR EncryptedValue
+// A PipelineEnvironmentVariable maps to a "[Environment variable]" object.
+//
+// [Environment variable]: https://api.gocd.org/current/#the-environment-variable-object
 type PipelineEnvironmentVariable struct {
 	Name           string `json:"name,omitempty"`
 	Value          string `json:"value,omitempty"`
@@ -35,6 +50,9 @@ type PipelineEnvironmentVariable struct {
 	Secure         bool   `json:"secure,omitempty"`
 }
 
+// A PipelineStage maps to the "[stage]" object.
+//
+// [stage]: https://api.gocd.org/current/#the-stage-object
 type PipelineStage struct {
 	Name                  string                        `json:"name,omitempty"`
 	FetchMaterial         bool                          `json:"fetch_material,omitempty"`
@@ -45,17 +63,26 @@ type PipelineStage struct {
 	Jobs                  []PipelineJob                 `json:"jobs,omitempty"`
 }
 
+// A PipelineStageApproval maps to the "[approval]" object.
+//
+// [approval]: https://api.gocd.org/current/#the-approval-object
 type PipelineStageApproval struct {
 	Type               string                              `json:"type,omitempty"`
 	AllowOnlyOnSuccess bool                                `json:"allow_only_on_success,omitempty"`
 	Authorization      *PipelineStageApprovalAuthorization `json:"authorization,omitempty"`
 }
 
+// A PipelineStageApprovalAuthorization maps to the "[authorization]" object.
+//
+// [authorization]: https://api.gocd.org/current/#the-authorization-object
 type PipelineStageApprovalAuthorization struct {
 	Users []string `json:"users,omitempty"`
 	Roles []string `json:"roles,omitempty"`
 }
 
+// A PipelineJob maps to the "[job]" object.
+//
+// [job]: https://api.gocd.org/current/#the-job-object
 type PipelineJob struct {
 	Name                 string                        `json:"name,omitempty"`
 	RunInstanceCount     string                        `json:"run_instance_count,omitempty"`
@@ -68,12 +95,18 @@ type PipelineJob struct {
 	ElasticProfileId     string                        `json:"elastic_profile_id,omitempty"`
 }
 
+// A PipelineTask maps to the "[task]" object.
+//
+// [task]: https://api.gocd.org/current/#the-task-object
 type PipelineTask struct {
 	Type       string                  `json:"type,omitempty"`
 	Attributes *PipelineTaskAttributes `json:"attributes,omitempty"`
 }
 
-// TODO - Validation on arguments passed and types
+// A PipelineTaskAttributes maps to the various types of [task attributes]. See
+// the documentation for which attributes need configuring for which task types.
+//
+// [task attributes]: https://api.gocd.org/current/#the-exec-task-attributes
 type PipelineTaskAttributes struct {
 	Arguments           []string                              `json:"arguments,omitempty"`
 	ArtifactId          string                                `json:"artifact_id,omitempty"`
@@ -92,16 +125,15 @@ type PipelineTaskAttributes struct {
 	WorkingDirectory    string                                `json:"working_directory,omitempty"`
 }
 
-// Duplicate of PipelineTask
-// Required to avoid recursive struct for OnCancel
+// Duplicate of PipelineTask which is required to avoid recursive struct for
+// OnCancel.
 type PipelineCancelTask struct {
 	Type       string                        `json:"type,omitempty"`
 	Attributes *PipelineCancelTaskAttributes `json:"attributes,omitempty"`
 }
 
-// Duplicate of PipelineTaskAttributes
-// Required to avoid recursive struct for OnCancel
-// TODO - Validation on arguments passed and types
+// Duplicate of PipelineTaskAttributes which is required to avoid recursive
+// struct for OnCancel.
 type PipelineCancelTaskAttributes struct {
 	Arguments           []string                              `json:"arguments,omitempty"`
 	ArtifactId          string                                `json:"artifact_id,omitempty"`
@@ -119,16 +151,26 @@ type PipelineCancelTaskAttributes struct {
 	WorkingDirectory    string                                `json:"working_directory,omitempty"`
 }
 
+// A PluginAttributePluginConfiguration is used to parse the object required
+// by the [pluggable task] object.
+//
+// [pluggable task]: https://api.gocd.org/current/#the-pluggable-task-object
 type PipelineAttributePluginConfiguration struct {
 	Id      string `json:"id,omitempty"`
 	Version string `json:"version,omitempty"`
 }
 
+// A PipelineTab maps to the [tab] object.
+//
+// [tab]: https://api.gocd.org/current/#the-tab-object
 type PipelineTab struct {
 	Name string `json:"name,omitempty"`
 	Path string `json:"path,omitempty"`
 }
 
+// A PipelineArtifact maps to the [pipeline config artifact] object.
+//
+// [pipeline config artifact]: https://api.gocd.org/current/#the-pipeline-config-artifact-object
 type PipelineArtifact struct {
 	Type          string                  `json:"type,omitempty"`
 	Source        string                  `json:"source,omitempty"`
@@ -138,26 +180,44 @@ type PipelineArtifact struct {
 	Configuration []PipelineConfiguration `json:"configuration,omitempty"`
 }
 
+// A PipelineConfiguration maps to the configuration field for the [pipeline
+// config artifact] object.
+//
+// [pipeline config artifact]: https://api.gocd.org/current/#the-pipeline-config-artifact-object
 type PipelineConfiguration struct {
 	Key   string `json:"key,omitempty"`
 	Value string `json:"value,omitempty"`
 }
 
+// A PipelineTrackingTool maps to the [tracking tool] object.
+//
+// [tracking tool]: https://api.gocd.org/current/#the-tracking-tool-object
 type PipelineTrackingTool struct {
 	Type       string                          `json:"type,omitempty"`
 	Attributes *PipelineTrackingToolAttributes `json:"attributes,omitempty"`
 }
 
+// A PipelineTrackingToolAttributes is used to handle the attributes object of
+// the [tracking tool] object.
+//
+// [tracking tool]: https://api.gocd.org/current/#the-tracking-tool-object
 type PipelineTrackingToolAttributes struct {
 	UrlPattern string `json:"url_pattern,omitempty"`
 	Regex      string `json:"regex,omitempty"`
 }
 
+// A PipelineTimer maps to the [timer] object.
+//
+// [timer]: https://api.gocd.org/current/#the-timer-object
 type PipelineTimer struct {
 	Spec          string `json:"spec,omitempty"`
 	OnlyOnChanges bool   `json:"only_on_changes,omitempty"`
 }
 
+// A PipelineCreateRequest is used to wrap the CreatePipeline request object
+// to be used by the [Create a pipeline] endpoint.
+//
+// [Create a pipeline]: https://api.gocd.org/current/#create-a-pipeline
 type PipelineCreateRequest struct {
 	Group    string   `json:"group,omitempty"`
 	Pipeline Pipeline `json:"pipeline,omitempty"`
